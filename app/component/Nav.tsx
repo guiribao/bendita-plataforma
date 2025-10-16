fe4f4c0842eae2578f737b6adb69204dcf9d5720
guiribao <gui.srosa@gmail.com>
2025-10-16 01:13:00 -0300

    Demonstra apelido user logado no header

--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -1,10 +1,10 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import pegarPerfilPeloIdUsuario from "~/domain/Perfil/perfil-pelo-id-usuario.server";
+import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import pegarPerfilPeloIdUsuario from '~/domain/Perfil/perfil-pelo-id-usuario.server';
 
-import { authenticator } from "~/secure/authentication.server";
+import { authenticator } from '~/secure/authentication.server';
 
-import userImage from "~/assets/img/user.png";
+import userImage from '~/assets/img/user.png';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   let usuario = await authenticator.isAuthenticated(request);
@@ -19,55 +19,51 @@ export default function Nav() {
   let { perfil, usuario } = useLoaderData();
 
   return (
-    <div className="menu">
-      <div className="menu-nav">
-        <div className="menu-toggle">
-          <i className="las la-bars"></i>
+    <div className='menu'>
+      <div className='menu-nav'>
+        <div className='menu-toggle'>
+          <i className='las la-bars'></i>
         </div>
-        <ul className="menu-list">
+        <ul className='menu-list'>
           <li>
-            <Link to="/sobre">Sobre</Link>
+            <Link to='/sobre'>Sobre</Link>
           </li>
           <li>
-            <Link to="/servicos">Serviços</Link>
+            <Link to='/servicos'>Serviços</Link>
           </li>
           <li>
-            <Link to="/conhecimento">Conhecimento</Link>
+            <Link to='/conhecimento'>Conhecimento</Link>
           </li>
           <li>
-            <Link to="/contato">Contato</Link>
+            <Link to='/contato'>Contato</Link>
           </li>
         </ul>
       </div>
 
       {!usuario && (
         <div>
-          <div className="menu-actions">
-            <Link to="/cadastro/basico">Associe-se</Link>
-            <Link to="/autentica/entrar">Entrar</Link>
+          <div className='menu-actions'>
+            <Link to='/cadastro/basico'>Associe-se</Link>
+            <Link to='/autentica/entrar'>Entrar</Link>
           </div>
         </div>
       )}
       {usuario && perfil && (
-        <div className="user-wrapper">
+        <div className='user-wrapper'>
           <img
             src={userImage}
-            alt="some random user image"
-            width={"40px"}
-            height={"40px"}
+            alt='some random user image'
+            width={'40px'}
+            height={'40px'}
           />
           <div>
-            <h6>
-              {perfil?.nome
-                ? `${perfil?.nome} ${perfil?.sobrenome}`
-                : usuario?.email}
-            </h6>
+            <h6>{perfil?.apelido || usuario?.email}</h6>
             <small>
-              <Link to="/app/perfil">Meu perfil</Link>
-              {" | "}
+              <Link to='/app/perfil'>Meu perfil</Link>
+              {' | '}
             </small>
             <small>
-              <Link to="/autentica/sair">Sair</Link>
+              <Link to='/autentica/sair'>Sair</Link>
             </small>
           </div>
         </div>
